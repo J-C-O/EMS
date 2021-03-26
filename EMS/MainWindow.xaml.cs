79c8b5c813ab04117f031c0a899bf006ce09ff91
J-C-O <47179422+J-C-O@users.noreply.github.com>
2021-03-26 12:04:28 +0100

    GUI überarbeitet: Strukturierung und Trennung der Sichten

--- EMS/MainWindow.xaml.cs
+++ EMS/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Windows;
-using EMSFactorClient;
+﻿using System.Windows;
+using EMS.ViewModels;
 
 namespace EMS
 {
@@ -10,18 +8,10 @@ namespace EMS
     /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Objekt vom Typ Client um mit einem Faktorbaum arbeiten zu können.
-        /// </summary>
-        Client client;
-        /// <summary>
-        /// Objekt vom Typ OpenFileDialog zum Laden von XML-Konfigurationen.
-        /// </summary>
-        OpenFileDialog openFile = new OpenFileDialog();
-        /// <summary>
-        /// Objekt vom Typ SaveFileDialog zum Speichern von XML-Konfigurationen.
-        /// </summary>
-        SaveFileDialog saveFile = new SaveFileDialog();
+        
+
+        ManageTreeViewModel manageView;
+        EditTreeViewModel editView;
 
         /// <summary>
         /// Konstruktor der Klasse.
@@ -30,82 +20,42 @@ namespace EMS
         {
             InitializeComponent();
 
-            //Client-Instanz mit übergebenem TextBlock zur Ausgabe
-            client = new Client(tbOutPut);
+            
 
-            //Setzen des Initialverzeichniss und des Dateifilters
-            openFile.InitialDirectory = "c:\\";
-            openFile.Filter = "xml files (*.xml)|*.xml";
-
-            //Setzen des Initialverzeichniss und des Dateifilters sowie den Standardnamen im Format config_<TIMESTAMP>.xml
-            saveFile.InitialDirectory = "c:\\";
-            saveFile.Filter = "xml files (*.xml)|*.xml";
-            saveFile.FileName = "config_" + DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            //Views initialisieren
+            editView = new EditTreeViewModel();
+            manageView = new ManageTreeViewModel();
         }
 
+        
         /// <summary>
-        /// Initialisiert einen Faktorbaum.
+        /// Öffnet die "Manage Tree" Ansicht.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void buttonInitialize_Click(object sender, RoutedEventArgs e)
+        private void button_ManageTree_Click(object sender, RoutedEventArgs e)
         {
-            client.Initialize();
+            DataContext = manageView;
         }
 
         /// <summary>
-        /// Schaltet einen Faktorbaum weiter.
+        /// Öffnet die "Edit Tree" Ansicht.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void buttonGetNext_Click(object sender, RoutedEventArgs e)
+        private void button_EditTree_Click(object sender, RoutedEventArgs e)
         {
-            client.Next();
+            DataContext = editView;
         }
 
         /// <summary>
-        /// Speichert eine Faktorbaumkonfiguration ab.
-        /// Hierfür wird ein Dialogfenster geöffnet.
+        /// Beendet die Anwendung
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        private void button_Close_Click(object sender, RoutedEventArgs e)
         {
-            string savePath;
-            saveFile.ShowDialog();
-
-            savePath = saveFile.FileName;
-            textBoxSave.Text = savePath;
-            client.WriteConfig(savePath);
-        }
-
-        /// <summary>
-        /// Lädt eine Faktorbaumkonfiguration.
-        /// Hierfür wird ein Dialogfenster geöffnet.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonLoad_Click(object sender, RoutedEventArgs e)
-        {
-            string loadPath;
-            if (openFile.ShowDialog() == true)
-            {
-                loadPath = openFile.FileName;
-
-                textBoxLoad.Text = loadPath;
-                client.LoadConfig(loadPath);
-            }
-
-        }
-
-        /// <summary>
-        /// Gibt eine aktuelle Faktorbaumkonfiguration aus.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonPrint_Click(object sender, RoutedEventArgs e)
-        {
-            client.PrintTreeGUI();
+            this.Close();
         }
     }
 }
---
+++ EMS/Views/ManageTreeView.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using EMSFactorClient;
+using Microsoft.Win32;
+
+namespace EMS.Views
+{
+    /// <summary>
+    /// Interaktionslogik für ManageTreeView.xaml
+    /// </summary>
+    public partial class ManageTreeView : UserControl
+    {
+        /// <summary>
+        /// Objekt vom Typ Client um mit einem Faktorbaum arbeiten zu können.
+        /// </summary>
+        Client client;
+        /// <summary>
+        /// Objekt vom Typ OpenFileDialog zum Laden von XML-Konfigurationen.
+        /// </summary>
+        OpenFileDialog openFile = new OpenFileDialog();
+        /// <summary>
+        /// Objekt vom Typ SaveFileDialog zum Speichern von XML-Konfigurationen.
+        /// </summary>
+        SaveFileDialog saveFile = new SaveFileDialog();
+        public ManageTreeView()
+        {
+            InitializeComponent();
+
+            //Client-Instanz mit übergebenem TextBlock zur Ausgabe
+            client = new Client(tbOutPut);
+
+            //Setzen des Initialverzeichniss und des Dateifilters
+            openFile.InitialDirectory = "c:\\";
+            openFile.Filter = "xml files (*.xml)|*.xml";
+
+            //Setzen des Initialverzeichniss und des Dateifilters sowie den Standardnamen im Format config_<TIMESTAMP>.xml
+            saveFile.InitialDirectory = "c:\\";
+            saveFile.Filter = "xml files (*.xml)|*.xml";
+            saveFile.FileName = "config_" + DateTime.Now.ToString("yyyyMMddHHmmssffff");
+        }
+
+        /// <summary>
+        /// Lädt eine Faktorbaumkonfiguration.
+        /// Hierfür wird ein Dialogfenster geöffnet.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            string loadPath;
+            if (openFile.ShowDialog() == true)
+            {
+                loadPath = openFile.FileName;
+
+                tb_Load.Text = loadPath;
+                client.LoadConfig(loadPath);
+            }
+        }
+
+        /// <summary>
+        /// Speichert eine Faktorbaumkonfiguration ab.
+        /// Hierfür wird ein Dialogfenster geöffnet.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            string savePath;
+            saveFile.ShowDialog();
+
+            savePath = saveFile.FileName;
+            tb_Save.Text = savePath;
+            client.WriteConfig(savePath);
+        }
+
+        /// <summary>
+        /// Initialisiert einen Faktorbaum.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Initialize_Click(object sender, RoutedEventArgs e)
+        {
+            client.Initialize();
+        }
+
+        /// <summary>
+        /// Gibt eine aktuelle Faktorbaumkonfiguration aus.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Print_Click(object sender, RoutedEventArgs e)
+        {
+            client.PrintTreeGUI();
+        }
+
+        /// <summary>
+        /// Schaltet einen Faktorbaum weiter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Next_Click(object sender, RoutedEventArgs e)
+        {
+            client.Next();
+        }
+    }
+}
